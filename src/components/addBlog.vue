56640c104f3b2581bebad10c5840463d12ea518b
Francky Vercruysse <francky.vercruysse@outlook.com>
2017-12-23 18:55:22 +0100

    checkboxes tags added

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -6,12 +6,28 @@
         <input type="text" v-model.lazy="blog.title" required />
         <label>Blog Content</label>
         <textarea v-model.lazy="blog.content"></textarea>
+        <div id="checkboxes">
+            <label>Angular</label>
+            <input type="checkbox" value="Angula" v-model="blog.tags" />
+            <label>Vue.js</label>
+            <input type="checkbox" value="Vue.js" v-model="blog.tags" />
+            <label>Asp.NET</label>
+            <input type="checkbox" value="Asp.NET" v-model="blog.tags" />
+            <label>MongoDB</label>
+            <input type="checkbox" value="MongoDB" v-model="blog.tags" />
+            <label>Firebase</label>
+            <input type="checkbox" value="Firebase" v-model="blog.tags" />
+        </div>
     </form>
     <div id="preview">
         <h3>Preview Blog</h3>
         <p>Blog Title : {{blog.title}}</p>
         <p>Blog Content</p>
         <p>{{blog.content}}</p>
+        <p>Blog Tags :</p>
+        <ul>
+            <li v-for="tag in blog.tags">{{tag}}</li>
+        </ul>
     </div>
   </div>
 </template>
@@ -23,7 +39,8 @@ export default {
     return {
       blog:{
           title:'',
-          content:''
+          content:'',
+          tags:[]
       }
     }
   }
@@ -54,5 +71,14 @@ input[type="text"], textarea{
 }
 h3{
     margin-top: 10px;
+}
+
+#checkboxes input{
+    display: inline-block;
+    margin-right: 10px;
+}
+
+#checkboxes label{
+    display:inline-block;
 }
 </style>
