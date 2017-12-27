232b6f011bc0afcc6c1740bef28fd119bb39d9e4
Francky Vercruysse <francky.vercruysse@outlook.com>
2017-12-27 17:57:39 +0100

    setup routing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter(
+  { 
+    mode: 'history',
+  routes: Routes 
+})
+
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router                //router : router
 })
