dd83b74d8d3803c651363a8e241e7ecc3868e054
Francky Vercruysse <francky.vercruysse@outlook.com>
2018-01-22 03:01:29 +0100

    Test Consistency with category.vue and tag.vue component

--- src/components/categories/category.vue
+++ src/components/categories/category.vue
@@ -4,12 +4,24 @@
             <span>{{category.name}}</span>
             <span class="float-right mr-0 mr-sm-5" @click="deleteCategory(category)">
                 <icon name="trash"></icon>
-                <b-modal :id="category.id" title="Delete Category" size="sm" @ok="deleteCategoryConfirmed(category)" >
+                <b-modal 
+                    :id="category.id" 
+                    title="Delete Category" 
+                    size="sm"
+                    ok-title="confirm delete" 
+                    @ok="deleteCategoryConfirmed(category)" >
                         <p class="my-4">Are you sure you want to delete this item?</p>
+                </b-modal>
+                <b-modal :id="'not-allowed'+category.id" title="Delete Category not allowed" size="sm" ok-only>
+                        {{message}}
+                        <ul>
+                            <li v-for="(title,index) in postTitles" :key="`title-${index}`">{{title}}</li>
+                        </ul>
                 </b-modal>
             </span>
             <span class="float-right mr-4 mr-sm-5" @click="showForm(category)"><icon name="pencil"></icon></span>
         </b-list-group-item>
+        
         <div v-show="isEditing">
             <input type="text" v-model="newCat">
             <b-button variant="primary" size="sm" class="mr-3" @click="saveCategory(category)">save</b-button>
@@ -31,7 +43,9 @@ export default {
             payload : {
                 oldCategory:{},
                 newCategory:{}
-            }
+            },
+            message:'',
+            postTitles : [],
         }
     },
     components:{
@@ -39,9 +53,22 @@ export default {
     },
     methods:{
         deleteCategory(category){
-            this.$root.$emit('bv::show::modal',category.id);
+            this.$store.dispatch('checkDeleteCategoryConsistency',category.id)
+                .then(data =>{
+                    if(data.length==0){
+                        this.$root.$emit('bv::show::modal',category.id);
+                    }
+                    else{
+                        this.message = 'there are '+ data.length +' blog posts belonging to this category :'
+                        this.postTitles = data;
+                        this.$root.$emit('bv::show::modal','not-allowed'+category.id);
+                    }
+                })
         },
         deleteCategoryConfirmed(category){
+            this.$store.dispatch('deleteCategory',category);
+        },
+        deleteCategoryNotAllowed(category){
             this.$store.dispatch('deleteCategory',category);
         },
         showForm(category){
--- src/components/tags/tag.vue
+++ src/components/tags/tag.vue
@@ -4,8 +4,14 @@
             <span>{{tag.name}}</span>
             <span class="float-right mr-0 mr-sm-5" @click="deleteTag(tag)">
                 <icon name="trash"></icon>
-                <b-modal :id="tag.id" title="Delete Tag" size="sm" @ok="deleteTagConfirmed(tag)" >
+                <b-modal :id="tag.id" title="Delete Tag" size="sm" ok-title="confirm delete" @ok="deleteTagConfirmed(tag)" >
                         <p class="my-4">Are you sure you want to delete this item?</p>
+                </b-modal>
+                <b-modal :id="'not-allowed'+tag.id" title="Delete Tag not allowed" size="sm" ok-only>
+                        {{message}}
+                        <ul>
+                            <li v-for="(title,index) in postTitles" :key="`title-${index}`">{{title}}</li>
+                        </ul>
                 </b-modal>
             </span>
             <span class="float-right mr-4 mr-sm-5" @click="showForm(tag)"><icon name="pencil"></icon></span>
@@ -31,7 +37,9 @@ export default {
             payload : {
                 oldTag:{},
                 newTag:{}
-            }
+            },
+            message:'',
+            postTitles : [],
         }
     },
     components:{
@@ -39,7 +47,22 @@ export default {
     },
     methods:{
         deleteTag(tag){
-            this.$root.$emit('bv::show::modal',tag.id);
+             this.$store.dispatch('checkDeleteTagConsistency',tag.id)
+                .then(data =>{
+                    if(data.length==0){
+                        this.$root.$emit('bv::show::modal',tag.id);
+                    }
+                    else{
+                        if (data.length==1){
+                            this.message = 'there is '+ data.length +' blog post that belongs to this tag :'
+                        }
+                        else {
+                            this.message = 'there are '+ data.length +' blog posts belonging to this tag :'
+                        }
+                        this.postTitles = data;
+                        this.$root.$emit('bv::show::modal','not-allowed'+tag.id);
+                    }
+                })
         },
         deleteTagConfirmed(tag){
             this.$store.dispatch('deleteTag',tag);
